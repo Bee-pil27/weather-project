ca696241a7b963347a1e3dd07257bba9d5073394
Bee-pil27 <Bongekah11@gmail.com>
2024-12-30 17:49:42 +0200

    Started integrating the API.

--- src/index.js
+++ src/index.js
@@ -58,31 +58,36 @@ function handleSearchSubmit(event) {
 }
 function getForecast(city) {
   let apiKey = "3f2d7e74630cb3758ac3e06t5aof40f8";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
 }
 
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
   <div class="weather-forecast-date">${day}</div>
-  <div class="weather-forecast-icon">⛅</div>
+
+  <img src="${day.condition.icon_url}" class="weather-forecast-icon" /> 
+  </div>
   <div class="weather-forecast-temperatures">
   <div class="weather-forecast-temperature">
-    <strong> 15°</strong>
+    <strong>${Math.round(day.temperature.maximum)} </strong>
   </div>
-  <div class="weather-forecast-temperature">9°</div>
+  <div class="weather-forecast-temperature"> ${Math.round(
+    day.temperature.minimum
+  )} °</div>
 </div>
 </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
