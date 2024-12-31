6bc3486de24fe1c1479618b914531d1e6bcf9245
Bee-pil27 <Bongekah11@gmail.com>
2024-12-31 13:01:58 +0200

    Updated the way days element show up.

--- src/index.js
+++ src/index.js
@@ -56,6 +56,14 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "3f2d7e74630cb3758ac3e06t5aof40f8";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -73,17 +81,17 @@ function displayForecast(response) {
         forecastHtml +
         `
       <div class="weather-forecast-day">
-  <div class="weather-forecast-date">${day}</div>
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
 
   <img src="${day.condition.icon_url}" class="weather-forecast-icon" /> 
   </div>
   <div class="weather-forecast-temperatures">
   <div class="weather-forecast-temperature">
-    <strong>${Math.round(day.temperature.maximum)} </strong>
+    <strong>${Math.round(day.temperature.maximum)}°</strong>
   </div>
   <div class="weather-forecast-temperature"> ${Math.round(
     day.temperature.minimum
-  )} °</div>
+  )}°</div>
 </div>
 </div>
     `;
